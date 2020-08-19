b6543b06e84bac6ef93444b0d1cc2feb8d4bb06a
zzq621 <1779176876@qq.com>
2020-08-19 09:16:18 +0800

    eureka集群生产者8002

---
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,40 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.CommonResult;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class PaymentController {
+    @Autowired
+    private PaymentService paymentService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @PostMapping("/payment/create")
+    public CommonResult create(@RequestBody Payment payment){
+        int i = paymentService.create(payment);
+        if(i>0){
+            return new CommonResult(200,"插入成功"+serverPort,i);
+        }else{
+            return new CommonResult(404,"插入失败",null);
+        }
+    }
+
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        Payment payment = paymentService.getPaymentById(id);
+        if(payment!=null){
+            return new CommonResult(200,"查询成功"+serverPort,payment);
+        }else{
+            return new CommonResult(404,"查询失败",null);
+        }
+    }
+
+}
