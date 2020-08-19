d2097ceeab879115e9cb8e34b5bacacc24f8e464
zzq621 <1779176876@qq.com>
2020-08-19 14:06:33 +0800

    gateway路由网关配置9527

---
+++ cloud-gateway-gateway9527/src/test/java/TimeTest.java
@@ -0,0 +1,8 @@
+import java.time.ZonedDateTime;
+
+public class TimeTest {
+    public static void main(String[] args) {
+        ZonedDateTime time = ZonedDateTime.now();
+        System.out.println(time);
+    }
+}
